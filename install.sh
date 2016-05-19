d36b6d17dfe26a9373f9a6f40bbbb15df8aedc65
Thomas Citharel <tcit@tcit.fr>
2016-05-19 20:13:00 +0200

    make travis work

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+if ["$TRAVIS" == "noassets"]; then
+    composer install --no-interaction --no-progress --prefer-dist -o
+else
+    echo "Installing PHP dependencies through Composer..."
+    composer install
+    echo "Downloading javascript librairies through npm..."
+    npm install
+    echo "Downloading fonts librairies through bower..."
+    bower install
+    echo "Concat, minify and installing assets..."
+    grunt
+fi
