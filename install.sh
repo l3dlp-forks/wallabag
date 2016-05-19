0547f7bc19107591c3cfed669a485579741bdc0a
Thomas Citharel <tcit@tcit.fr>
2016-05-19 21:03:37 +0200

    impove travis & update deps

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/env bash
 
-set -ev
-
-if [[ $TRAVIS == 'noassets' ]]; then
+if [[ $ASSETS == 'nobuild' ]]; then
     composer install --no-interaction --no-progress --prefer-dist -o
 else
     echo "Installing PHP dependencies through Composer..."
@@ -10,7 +8,7 @@ else
     echo "Downloading javascript librairies through npm..."
     npm install
     echo "Downloading fonts librairies through bower..."
-    bower install
+    bower install -q
     echo "Concat, minify and installing assets..."
     grunt
 fi
