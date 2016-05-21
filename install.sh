aa58a152fd9ddd47015259cc60affd03cf861813
Thomas Citharel <tcit@tcit.fr>
2016-05-21 16:17:41 +0200

    fix travis build

--- install.sh
+++ install.sh
@@ -3,9 +3,15 @@
 if [[ $ASSETS == 'nobuild' ]]; then
     composer install --no-interaction --no-progress --prefer-dist -o
 else
-    echo "Installing PHP dependencies through Composer..."
-    SYMFONY_ENV=prod composer install --no-dev -o --prefer-dist
 
+    echo "Installing PHP dependencies through Composer..."
+    if [[ $ASSETS == 'build' ]]; then
+        composer install --no-interaction --no-progress --prefer-dist -o
+    else
+        SYMFONY_ENV=prod composer install --no-dev -o --prefer-dist
+    fi
+
+    chmod ugo+x vendor/mouf/nodejs-installer/bin/local/npm
     echo "Downloading javascript librairies through npm..."
     vendor/mouf/nodejs-installer/bin/local/npm install
 
